b0b1646117f0c63f0d05b258311cfbe45333dc8a
prince-0203 <mc.prince.0203@gmail.com>
2016-05-08 09:09:21 +0900

    フォルダ構成を変更

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,44 @@ const gulp = require('gulp'),
       notify = require('gulp-notify'),
       sourcemaps = require('gulp-sourcemaps'),
       rimraf = require('rimraf'),
-      runSequence = require('run-sequence');
+      runSequence = require('run-sequence'),
+      replace = require('gulp-replace'),
+      pkg = require('./package.json');
+
+const copyList = [
+  'icon/icon.png',
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/material-design-icons/iconfont/**/*'
+];
 
 gulp.task('clean', function(callback) {
   rimraf('dist', callback);
+});
+
+gulp.task('manifest', function() {
+  return gulp.src('manifest.json')
+    .pipe(replace('{{version}}', pkg.version))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('locale', function() {
+  return gulp.src('_locales/**/*', { base: '.' })
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src(copyList)
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/html/**/*.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('sass', function() {
@@ -21,20 +55,19 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(gulp.dest('dist/'));
-});
-
 gulp.task('build', (callback) => {
   runSequence(
     'clean',
-    ['sass', 'js'],
+    ['manifest', 'locale', 'copy', 'html', 'js', 'sass'],
     callback
   );
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('manifest.json', ['manifest']);
+  gulp.watch('_locales/**/*', ['locale']);
+  gulp.watch(copyList, ['copy']);
+  gulp.watch('src/html/**/*.html', ['html']);
   gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/sass/**/*.scss', ['sass']);
 });
